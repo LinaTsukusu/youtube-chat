af2d0bcd8f99e939df62e64ca0bb74a9910bc842
neverclear <neverclear86sab@gmail.com>
2019-10-10 22:44:13 +0900

    取得時間あたり修正

--- src/live-comment.ts
+++ src/live-comment.ts
@@ -12,7 +12,8 @@ interface CommentItem {
 export class LiveComment extends EventEmitter {
   public readonly channelId?: string
   public liveId?: string
-  public interval = 1000 * 10
+  public interval = 1000
+  private prevTime = Date.now()
   private readonly headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
   private observer?: NodeJS.Timer
 
@@ -32,13 +33,16 @@ export class LiveComment extends EventEmitter {
       const liveRes = await axios.get(`https://www.youtube.com/channel/${this.channelId}/live`, {headers: this.headers})
       this.liveId = liveRes.data.match(/"watchEndpoint":{"videoId":"(\w*)"}/gm)[0].match(/"videoId":"(.*)"/)[1] as string
     }
+    // TODO スパチャとか対応
+    // TODO 情報量増やす
+    // TODO 時間でfilterじゃなくてsliceする
     this.observer = setInterval(async () => {
       const res = await axios.get(`https://www.youtube.com/live_chat?v=${this.liveId}&pbj=1`, {headers: this.headers})
-      const now = new Date().valueOf() - this.interval
+      // console.log(res)
       const items = res.data[1].response.contents.liveChatRenderer.actions.slice(0, -1)
       items.filter((v: any) => {
           try {
-            return new Date(Number(v.addChatItemAction.item.liveChatTextMessageRenderer.timestampUsec) / 1000).valueOf() >= now
+            return LiveComment.usecToTime(v.addChatItemAction.item.liveChatTextMessageRenderer.timestampUsec) >= this.prevTime
           } catch (e) {
             return false
           }
@@ -48,11 +52,16 @@ export class LiveComment extends EventEmitter {
           return {
             authorName: item.authorName.simpleText,
             message: item.message.runs[0].text,
-            time: new Date(Number(v.addChatItemAction.item.liveChatTextMessageRenderer.timestampUsec) / 1000),
+            time: new Date(LiveComment.usecToTime(v.addChatItemAction.item.liveChatTextMessageRenderer.timestampUsec)),
           }
         }).forEach((v: CommentItem) => {
           this.emit('comment', v)
+          this.prevTime = v.time.getTime() + 1
         })
     }, this.interval)
+  }
+
+  private static usecToTime(usec: string): number {
+    return Number(usec) / 1000
   }
 }
