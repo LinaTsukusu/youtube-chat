26ccc4488231548b5c05fc7a15f7cce75b4cf41e
Lina Tsukusu <neverclear86sab@gmail.com>
2021-11-27 12:05:25 +0900

    add: requests.ts test file

---
+++ test/requests.test.ts
@@ -0,0 +1,22 @@
+import { fetchLivePage, fetchChat } from "../src/requests"
+import axios from "axios"
+
+jest.mock("axios")
+
+describe("requests", () => {
+  describe("fetchChat", () => {
+    test("Request", () => {
+
+    })
+  })
+
+  describe("fetchLivePage", () => {
+    test("ChannelID request", () => {
+
+    })
+
+    test("LiveID request", () => {
+
+    })
+  })
+})
