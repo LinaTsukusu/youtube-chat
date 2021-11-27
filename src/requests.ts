bc3b1a5e4dabe316148d1926beac2bd2dec10814
Lina Tsukusu <neverclear86sab@gmail.com>
2021-11-27 14:11:54 +0900

    fix: Use parser in requests

--- src/requests.ts
+++ src/requests.ts
@@ -1,27 +1,28 @@
-import { FetchOptions, GetLiveChatResponse } from "./types/yt-response"
 import axios from "axios"
+import { parseChatData, getOptionsFromLivePage } from "./parser"
+import { FetchOptions } from "./types/yt-response"
+import { ChatItem } from "./types/data"
 
-export async function fetchChat(options: FetchOptions): Promise<GetLiveChatResponse> {
+export async function fetchChat(options: FetchOptions): Promise<[ChatItem[], string]> {
   const url = `https://www.youtube.com/youtubei/v1/live_chat/get_live_chat?key=${options.apiKey}`
-
-  return (
-    await axios.post(url, {
-      context: {
-        client: {
-          clientVersion: options.clientVersion,
-          clientName: "WEB",
-        },
+  const res = await axios.post(url, {
+    context: {
+      client: {
+        clientVersion: options.clientVersion,
+        clientName: "WEB",
       },
-      continuation: options.continuation,
-    })
-  ).data
+    },
+    continuation: options.continuation,
+  })
+
+  return parseChatData(res.data)
 }
 
-export async function fetchLivePage(id: { channelId: string } | { liveId: string }): Promise<string> {
+export async function fetchLivePage(id: { channelId: string } | { liveId: string }): Promise<FetchOptions> {
   const url =
     "channelId" in id
       ? `https://www.youtube.com/channel/${id.channelId}/live`
       : `https://www.youtube.com/watch?v=${id.liveId}`
-
-  return (await axios.get(url)).data.toString()
+  const res = await axios.get(url)
+  return getOptionsFromLivePage(res.data.toString())
 }
